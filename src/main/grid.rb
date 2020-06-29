ec002d0fab943c752502719e7ab0ae6aed3331ca
mauricioabisay <mauricioabisay.lopez@gmail.com>
2020-06-29 15:41:59 -0500

    update Grid class add cells matrix

--- src/main/grid.rb
+++ src/main/grid.rb
@@ -1,7 +1,12 @@
 class Grid
-  attr_accessor :rows, :cols
+  attr_accessor :rows, :cols, :cells
   def initialize(rows = 3, cols = 3)
     @rows = rows
     @cols = cols
+    @cells = Array.new(@rows) do |row|
+               Array.new(@cols) do |col|
+                 Cell.new(row, col)
+               end
+             end
   end
 end
