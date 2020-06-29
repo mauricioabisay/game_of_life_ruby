4a09462b67de04a714324343d223b7ab1ef11e77
mauricioabisay <mauricioabisay.lopez@gmail.com>
2020-06-29 15:31:10 -0500

    update Cell class

--- src/main/cell.rb
+++ src/main/cell.rb
@@ -1,2 +1,12 @@
 class Cell
+  attr_accessor :row, :col, :alive
+  def initialize(row = 0, col = 0, alive = false)
+    @row = row
+    @col = col
+    @alive = alive
+  end
+
+  def isAlive?
+    @alive
+  end
 end
