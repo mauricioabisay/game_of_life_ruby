11dc27e8d6fd5e9ccd84dcf56755d147238cf7b3
mauricioabisay <mauricioabisay.lopez@gmail.com>
2020-06-29 18:23:48 -0500

    update GameOfLife class wrapper add grid and seed alive cells array

--- src/main/gol.rb
+++ src/main/gol.rb
@@ -1,2 +1,12 @@
+require_relative './grid.rb'
+
 class GameOfLife
+  attr_accessor :grid, :seedCells
+  def initialize(grid = Grid.new, seedCells = Array.new)
+    @grid = grid
+    @seedCells = seedCells
+    @seedCells.each do |aliveCell|
+      @grid.cells[aliveCell[0]][aliveCell[1]].alive = true
+    end
+  end
 end
