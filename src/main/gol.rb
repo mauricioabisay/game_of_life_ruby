4e5701acd95ed263b663450e6bdc6dfe778c2012
mauricioabisay <mauricioabisay.lopez@gmail.com>
2020-06-29 21:04:34 -0500

    update GameOfLife class add rules for cells alive change status

--- src/main/gol.rb
+++ src/main/gol.rb
@@ -9,4 +9,24 @@ class GameOfLife
       @grid.cells[aliveCell[0]][aliveCell[1]].alive = true
     end
   end
+
+  def apply_rules
+    @grid.cells.each do |row|
+      row.each do |cell|
+        neighbours_counter = @grid.neighbours_alive_counter(cell)
+        if cell.isAlive? && neighbours_counter < 2
+          cell.alive = false
+        end
+        if cell.isAlive? && neighbours_counter > 3
+          cell.alive = false
+        end
+        if cell.isAlive? && ( neighbours_counter == 2 || neighbours_counter == 3 )
+          cell.alive = true
+        end
+        if !cell.isAlive? && neighbours_counter == 3
+          cell.alive = true
+        end
+      end
+    end
+  end
 end
